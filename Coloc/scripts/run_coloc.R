95497b35411157c0592c8a16c3c71f81d6410562
Raehoon Jeong <rjeong@g.harvard.edu>
2022-10-11 23:14:26 -0400

    Coloc updated

--- Coloc/scripts/run_coloc.R
+++ Coloc/scripts/run_coloc.R
@@ -1,46 +1,41 @@
 library(coloc, quietly=TRUE)
 library(argparser, quietly=TRUE)
 
-WriteTable <- function(data, filename, index.name) {
-    datafile <- file(filename, open = "wt")
-    on.exit(close(datafile))
-    #header <- c(index.name, colnames(data))
-    #writeLines(paste0(header, collapse="\t"), con=datafile, sep="\n")
-    write.table(data, datafile, sep="\t", row.names=FALSE, col.names=FALSE, quote=FALSE)
-}
-
-
 p <- arg_parser("Run coloc")
 p <- add_argument(p, "trait", help="")
 p <- add_argument(p, "peak", help="")
 
 argv <- parse_args(p)
 
+## Reading GWAS data
 gwas_for_coloc <- read.csv(gzfile(paste('/data/bulyk/Raehoon/QTL_o2/PU1-colocalization-analysis/Coloc/data/GWAS/',argv$trait,'_',argv$peak,'.gwas_input.txt.gz', sep='')), header=T, sep='\t', stringsAsFactors=F)
-#colnames(gwas) <- c("chr", "position", "A1", "A2", "MAF", "HWE-P", "iscore", "snp", "beta", "se", "p")
-#gwas["varbeta"] <- gwas$se^2
-#gwas_for_coloc <- as.list(gwas[c("beta", "varbeta", "snp", "position")])
+
+# Removing any duplicate variant positions
 gwas_for_coloc <- as.list(na.omit(gwas_for_coloc[!duplicated(gwas_for_coloc$position), ]))
+
+# Setting required parameters
 gwas_for_coloc$type <- "quant"
 gwas_for_coloc$sdY <- 1
 
-#gwas.res <- finemap.abf(dataset = gwas_for_coloc)
 
+## Reading PU.1 bQTL data
 bqtl_for_coloc <- read.csv(gzfile(paste('/data/bulyk/Raehoon/QTL_o2/PU1-colocalization-analysis/Coloc/data/PU1/',argv$peak,'.pu1_input.txt.gz', sep='')), header=T, sep='\t', stringsAsFactors=F)
-#colnames(bqtl) <- c("peakname", "chr", "start", "end", "strand", "numsnp", "distance", "snp", "chr2", "position", "position_end", "p", "beta", "lead")
-#bqtl["varbeta"] <- (bqtl$beta/qnorm(bqtl$p/2, lower.tail = FALSE))^2
-#bqtl_for_coloc <- as.list(bqtl[c("beta", "varbeta", "snp", "position")])
+
+# Removing any duplicate variant positions
 bqtl_for_coloc <- as.list(na.omit(bqtl_for_coloc[!duplicated(bqtl_for_coloc$position), ]))
+
+# Setting required parameters
 bqtl_for_coloc$type <- "quant"
 bqtl_for_coloc$sdY <- 1
-#bqtl.res <- finemap.abf(dataset = bqtl_for_coloc)
 
+
+## Running Coloc with p12 = 1e-6 to be conservative
 coloc.res <- coloc.abf(dataset1 = gwas_for_coloc, dataset2 = bqtl_for_coloc, p12 = 1e-6)
 
 final <- coloc.res$summary
 final["peak"] <- argv$peak
 final["trait"] <- argv$trait
 final <- t(final)
-#final <- cbind(argv$trait, argv$peak, coloc.res$summary)
 
+# Writing the result to file
 write.table(final, paste('/data/bulyk/Raehoon/QTL_o2/PU1-colocalization-analysis/Coloc/results/',argv$trait,'_',argv$peak,'.coloc.txt', sep=''), sep="\t", row.names=FALSE, col.names=TRUE, quote=FALSE)
